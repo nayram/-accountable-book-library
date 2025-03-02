09574cabebb3d4e7b7f7e02ea7b2516eac1cb3d6
Nayram Mensah <nayrammensah@gmail.com>
2025-03-02 21:17:33 +0100

    fix(tests): fix e2e tests

--- src/apps/api/routes/index.ts
+++ src/apps/api/routes/index.ts
@@ -14,8 +14,8 @@ import { getCatalogsRequestSchema } from './catalogs/get-catalogs-request';
 const routes = Router();
 
 routes.post('/catalogs', SchemaValidator.body(postCreateCatalogRequestSchema), postCreateCatalogController);
+routes.get('/catalogs/search', SchemaValidator.query(getCatalogsRequestSchema), getCatalogsController);
 routes.get('/catalogs/:id', getCatalogByIdController);
-routes.get('/catologs/search', SchemaValidator.query(getCatalogsRequestSchema), getCatalogsController);
 routes.delete('/catalogs/:id', deleteCatalogByIdController);
 
 export default routes;
